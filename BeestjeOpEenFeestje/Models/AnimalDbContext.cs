18fcb0419c64c7a7f0b7b5746711db6668e66255
Kevin <k.janssen11@student.avans.nl>
2025-01-20 19:38:26 +0100

    database made

---
+++ BeestjeOpEenFeestje/Models/AnimalDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeestjeOpEenFeestje.Models
+{
+    public class AnimalDbContext : IdentityDbContext<AppUser>
+    {
+        public AnimalDbContext(DbContextOptions<AnimalDbContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<Animal> Animals { get; set; }
+        DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ BeestjeOpEenFeestje/Models/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace BeestjeOpEenFeestje.Models
+{
+    public static class ErrorMessages
+    {
+        public const string RequiredField = "Dit veld is verplicht.";
+        public const string InvalidPhoneNumber = "Voer een geldig telefoonnummer in.";
+        public const string InvalidPrice = "Voer een geldige prijs in.";
+        public const string MaxLengthAddress = "Adres" + MaxLength;
+        public const string MaxLengthName = "Naam" + MaxLength;
+
+        private const string MaxLength = " mag maximaal {0} tekens bevatten.";
+    }
+}
