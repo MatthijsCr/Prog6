35cb4fc9fc17b519513fd8aadaf7e3a3135e137e
MatthijsCr <msj.cromwijk@student.avans.nl>
2025-01-21 17:16:25 +0100

    start of c made

---
+++ BeestjeOpEenFeestje/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿
+using BeestjeOpEenFeestje.Models;
+using BeestjeOpEenFeestje.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BumboApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, password, isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Home");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel registerModel)
+        {
+            if (ModelState.IsValid) {
+                AppUser user = new AppUser() { Address = registerModel.Address, UserName = registerModel.Username };
+                if (registerModel.Email != null)
+                {
+                    user.Email = registerModel.Email;
+                }
+                if (registerModel.PhoneNumber != null)
+                {
+                    user.PhoneNumber = registerModel.PhoneNumber;
+                }
+
+                var result = await _userManager.CreateAsync(user, registerModel.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(registerModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BeestjeOpEenFeestje/ViewModels/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeestjeOpEenFeestje.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public required string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Password { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public required string Address { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}
