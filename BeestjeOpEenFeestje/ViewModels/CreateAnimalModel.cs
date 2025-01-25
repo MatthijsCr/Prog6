9948dddd621a66f4206092d07d624ee570ee77ac
MatthijsCr <msj.cromwijk@student.avans.nl>
2025-01-25 16:13:43 +0100

    create werkt

--- BeestjeOpEenFeestje/ViewModels/CreateAnimalModel.cs
+++ BeestjeOpEenFeestje/ViewModels/CreateAnimalModel.cs
@@ -6,17 +6,17 @@ namespace BeestjeOpEenFeestje.ViewModels
     public class CreateAnimalModel
     {
         [Required(ErrorMessage = ErrorMessages.RequiredField)]
-        public required string ImageURL;
+        public required string ImageURL { get; set; }
 
         [Required(ErrorMessage = ErrorMessages.RequiredField)]
         [MaxLength(50, ErrorMessage = ErrorMessages.MaxLengthName)]
-        public required string Name;
+        public required string Name { get; set; }
 
         [Required(ErrorMessage = ErrorMessages.RequiredField)]
-        public required string Type;
+        public required string Type { get; set; }
 
         [Required(ErrorMessage = ErrorMessages.RequiredField)]
         [Range(0, double.MaxValue, ErrorMessage = ErrorMessages.InvalidPrice)]
-        public required double Price;
+        public required double Price { get; set; }
     }
 }
